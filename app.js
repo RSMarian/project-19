fdcfdfd7c6101d9bae0a1011a93d2519bf2b99d1
RSMarian <marianstefan1499@gmail.com>
2022-06-11 20:09:13 +0300

    added dynamic links height

--- app.js
+++ app.js
@@ -7,6 +7,23 @@
 const date = document.getElementById('date');
 date.innerHTML = new Date().getFullYear();
 // ********** close links ************
+const navToggle = document.querySelector('.nav-toggle')
+const linksContainer = document.querySelector('.links-container')
+const links = document.querySelector('.links')
+
+navToggle.addEventListener('click', function(){
+    // linksContainer.classList.toggle('show-links');
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const linksHeight = links.getBoundingClientRect().height;
+
+    if(containerHeight === 0){
+        linksContainer.style.height = `${linksHeight}px`
+    }
+    else{
+        linksContainer.style.height = 0;
+    }
+
+});
 
 // ********** fixed navbar ************
 
